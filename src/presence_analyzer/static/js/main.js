f8fdf08d24c91e2f3700d2a814a6e336f3d8db71
Lukasz Swierszcz <lukasz.swierszcz@stxnext.pl>
2014-08-06 14:45:06 +0200

    Formatted JS code.

--- src/presence_analyzer/static/js/main.js
+++ src/presence_analyzer/static/js/main.js
@@ -5,7 +5,7 @@
  * @returns Date Date object based on input value
  */
 function parseInterval(value) {
-    var result = new Date(1,1,1);
-    result.setMilliseconds(value*1000);
+    var result = new Date(1, 1, 1);
+    result.setMilliseconds(value * 1000);
     return result;
 }
