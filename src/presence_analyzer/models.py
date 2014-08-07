641bedd30aa8f606f562a0165d5c7c45207e2fd5
Lukasz Swierszcz <lukasz.swierszcz@stxnext.pl>
2014-08-07 12:40:41 +0200

    Written method for parsing users XML file.

--- src/presence_analyzer/models.py
+++ src/presence_analyzer/models.py
@@ -23,3 +23,38 @@ class User(object):
             return True
 
         return False
+
+    @staticmethod
+    def get_data():
+        """
+        Gets users data from XML.
+        """
+        users = []
+
+        if (
+            not os.path.isfile(settings.USERS_XML) and
+            not User.fetch_users_file()
+        ):
+            return users
+
+        with open(settings.USERS_XML, 'r') as xml_file:
+            xml = etree.parse(xml_file)
+            root = xml.getroot()
+
+            server = root.find('server')
+            server_url = '{0}://{1}'.format(
+                server.find('protocol').text,
+                server.find('host').text,
+            )
+
+            for user in root.find('users'):
+                users.append({
+                    'user_id': user.get('id'),
+                    'name': user.find('name').text,
+                    'image_url': '{0}{1}'.format(
+                        server_url,
+                        user.find('avatar').text
+                    ),
+                })
+
+        return users
